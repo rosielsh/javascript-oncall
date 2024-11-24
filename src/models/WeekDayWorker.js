456e0d80a8a5c1d42c62165b6349ecc94174fa31
suhwa <tnghk9611@naver.com>
2024-11-24 17:39:09 +0900

    feat(Worker): 2일 연속일 때 사용자를 처리하는 기능 구현

--- src/models/WeekDayWorker.js
+++ src/models/WeekDayWorker.js
@@ -1,4 +1,4 @@
-import WorkersValidator from "../validators/WorkersValidator";
+import WorkersValidator from "../validators/WorkersValidator.js";
 
 class WeekDayWorker {
   #workers;
@@ -10,6 +10,30 @@ class WeekDayWorker {
     this.#workers = workers;
     this.#processPointer = 0;
     this.#previousChangedOrders = [];
+  }
+
+  getCurrentWorker() {
+    return this.#workers[this.#processPointer];
+  }
+
+  possibleWorker() {
+    this.#processPointer++;
+
+    if (this.#processPointer === this.#workers.length) {
+      this.#processPointer = 0;
+    }
+  }
+
+  isExistPrevChangedOrder() {
+    return this.#previousChangedOrders.length > 0;
+  }
+
+  getPreviousChangedOrder() {
+    return this.#previousChangedOrders.pop();
+  }
+
+  pushChangedOrder(worker) {
+    this.#previousChangedOrders.push(worker);
   }
 }
 
