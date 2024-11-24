27fd2231087c13f0f0ef8ed26c81bed142b5eb7e
suhwa <tnghk9611@naver.com>
2024-11-24 15:24:12 +0900

    feat(Month): 각 달의 일 수 및 공휴일 저장

--- src/models/Month.js
+++ src/models/Month.js
@@ -5,12 +5,27 @@ class Month {
   #month;
   #startDay;
 
+  static PUPLIC_HOLIDAYS = {
+    1: [1],
+    3: [1],
+    5: [5],
+    6: [6],
+    8: [15],
+    10: [3, 9],
+    12: [25],
+  };
+  
+  static DAY_OF_MONTH = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
   constructor(month, startDay) {
     MonthValidator.validate(month);
     StartDayValidator.validate(startDay);
     this.#month = month;
     this.#startDay = startDay;
   }
+
+  // 평일인지 주말인지 알 수 있음
+  getDayType(day) {}
 }
 
 export default Month;
