9ab0d3c7dde612374a91fae70ed74606c92747cd
suhwa <tnghk9611@naver.com>
2024-11-24 17:37:36 +0900

    fix(view): print 형태 수정

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,32 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class OutputView {
-  static printEmergencySchedule(schedules) {
-    // 월, 일, 휴일 여부, 근무자 출력
-    Console.print();
+  static printEmergencySchedule(month, startDay, assigned) {
+    const logs = [];
+    const dayofWeek = ["월", "화", "수", "목", "금", "토", "일"];
+
+    const startPointer = {
+      월: 0,
+      화: 1,
+      수: 2,
+      목: 3,
+      금: 4,
+      토: 5,
+      일: 6,
+    };
+
+    let pointer = startPointer[`${startDay}`];
+    assigned.forEach(([type, worker], idx) => {
+      logs.push(`${month}월 ${idx + 1}일 ${dayofWeek[pointer % 7]}${type === "WH" ? "(휴일)" : ""} ${worker}`);
+      pointer++;
+    });
+
+    Console.print(logs.join("\n"));
+  }
+
+  static printError(message) {
+    Console.print(message);
+    Console.print("");
   }
 }
 
