03e55c22cc02c0d8abddfa98f9982162732e1be5
suhwa <tnghk9611@naver.com>
2024-11-24 14:52:48 +0900

    test(WeekDayWorkerTest): 비상근무 사원의 이름 길이 예외 테스트 작성

--- __tests__/models/WeekDayWorker.test.js
+++ __tests__/models/WeekDayWorker.test.js
@@ -6,4 +6,23 @@ describe("WeekDayWorker 클래스 테스트", () => {
 
     expect(() => new WeekDayWorker(workers)).toThrow("[ERROR]");
   });
+
+  test("평일 비상근무 사원의 닉네임은 최소 1자 ~ 최대 5자 가능하다", () => {
+    const workers = ["준팍", "도밥", "준", "루루루루루"];
+
+    expect(() => new WeekDayWorker(workers)).not.toThrow();
+  });
+
+  test("평일 비상근무 사원의 닉네임이 1자 ~ 5자를 벗어나고 문자가 아니라면 예외가 발생한다", () => {
+    const workers = [
+      ["", "준팍"],
+      ["준팍준팍준팍", "루루"],
+      ["루루", undefined],
+      ["루루", NaN],
+    ];
+
+    workers.forEach((worker) => {
+      expect(() => new WeekDayWorker(worker)).toThrow("[ERROR]");
+    });
+  });
 });
