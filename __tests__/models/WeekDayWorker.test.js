b3e3686fa388dce8b10dc1e1fd658be080a43187
suhwa <tnghk9611@naver.com>
2024-11-24 14:46:51 +0900

    test(WeekDayWorker): 클래스를 import 하도록 수정

--- __tests__/models/WeekDayWorker.test.js
+++ __tests__/models/WeekDayWorker.test.js
@@ -1,7 +1,9 @@
+import WeekDayWorker from "../../src/models/WeekDayWorker.js";
+
 describe("WeekDayWorker 클래스 테스트", () => {
   test("평일 비상근무 사원에는 중복된 사람이 있으면 안된다.", () => {
     const workers = ["준팍", "준팍"];
 
-    expect(() => WeekDayWorker(workers)).toThrow("[ERROR]");
+    expect(() => new WeekDayWorker(workers)).toThrow("[ERROR]");
   });
 });
