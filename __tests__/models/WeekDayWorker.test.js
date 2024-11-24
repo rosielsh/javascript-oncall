f472528193159250064ada7a541d359e2a749f22
suhwa <tnghk9611@naver.com>
2024-11-24 15:02:41 +0900

    feat(WorkersValidator): 근무자 수 유효성 검사 구현

--- __tests__/models/WeekDayWorker.test.js
+++ __tests__/models/WeekDayWorker.test.js
@@ -2,23 +2,23 @@ import WeekDayWorker from "../../src/models/WeekDayWorker.js";
 
 describe("WeekDayWorker 클래스 테스트", () => {
   test("평일 비상근무 사원에는 중복된 사람이 있으면 안된다.", () => {
-    const workers = ["준팍", "준팍"];
+    const workers = ["준팍", "준팍", '준팍', '준팍','준팍'];
 
     expect(() => new WeekDayWorker(workers)).toThrow("[ERROR]");
   });
 
   test("평일 비상근무 사원의 닉네임은 최소 1자 ~ 최대 5자 가능하다", () => {
-    const workers = ["준팍", "도밥", "준", "루루루루루"];
+    const workers = ["준팍", "도밥", "준", "루루루루루", '수아'];
 
     expect(() => new WeekDayWorker(workers)).not.toThrow();
   });
 
   test("평일 비상근무 사원의 닉네임이 1자 ~ 5자를 벗어나고 문자가 아니라면 예외가 발생한다", () => {
     const workers = [
-      ["", "준팍"],
-      ["준팍준팍준팍", "루루"],
-      ["루루", undefined],
-      ["루루", NaN],
+      ["", "준팍", "루루", "도밥", "고니"],
+      ["준팍준팍준팍", "루루", "도밥", "고니", "수아"],
+      ["루루", undefined, "도밥", "고니", "수아"],
+      ["루루", NaN, "도밥", "고니", "수아"],
     ];
 
     workers.forEach((worker) => {
--- src/validators/WorkersValidator.js
+++ src/validators/WorkersValidator.js
@@ -2,6 +2,7 @@ import { generateError } from "../utils/generateError.js";
 
 class WorkersValidator {
   static validate(workers) {
+    this.#validateCount(workers);
     this.#validateDuplicate(workers);
     this.#validateType(workers);
     this.#validateLength(workers);
@@ -22,6 +23,12 @@ class WorkersValidator {
   static #validateLength(workers) {
     if (workers.some((name) => name.length < 1 || name.length > 5)) {
       generateError("모든 근무자의 이름은 1자 ~ 5자 이내로 입력가능합니다.");
+    }
+  }
+
+  static #validateCount(workers) {
+    if (workers.length < 5 || workers.length > 35) {
+      generateError("근무자는 최소 5명 ~ 최대 35명 가능합니다.");
     }
   }
 }
