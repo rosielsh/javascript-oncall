8aacd25086e935bcd36b95a6c40e3e410cfe6bd2
suhwa <tnghk9611@naver.com>
2024-11-24 14:33:43 +0900

    feat(views): InputView, OutputView 구현

---
+++ src/views/InputView.js
@@ -0,0 +1,15 @@
+class InputView {
+  static readMonthInfo() {
+    return Console.readLineAsync("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+  }
+
+  static readWeekDayWorker() {
+    return Console.readLineAsync("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+  }
+
+  static readDayOffWorker() {
+    return Console.readLineAsync("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+  }
+}
+
+export default InputView;
---
+++ src/views/OutputView.js
@@ -0,0 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class OutputView {
+  static printEmergencySchedule(schedules) {
+    // 월, 일, 휴일 여부, 근무자 출력
+    Console.print();
+  }
+}
+
+export default OutputView;
