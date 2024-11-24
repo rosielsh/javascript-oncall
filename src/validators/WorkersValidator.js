8336824d2db23b2c20839ccb45218e9c98e9e3b5
suhwa <tnghk9611@naver.com>
2024-11-24 14:56:36 +0900

    feat(WorkersValidator): 근무자의 이름의 타입과 길이 유효성 검사 구현

--- src/validators/WorkersValidator.js
+++ src/validators/WorkersValidator.js
@@ -3,11 +3,25 @@ import { generateError } from "../utils/generateError.js";
 class WorkersValidator {
   static validate(workers) {
     this.#validateDuplicate(workers);
+    this.#validateType(workers);
+    this.#validateLength(workers);
   }
 
   static #validateDuplicate(workers) {
     if (new Set(workers).size !== workers.length) {
       generateError("중복된 근무자를 입력할 수 없습니다.");
+    }
+  }
+
+  static #validateType(workers) {
+    if (!workers.every((name) => typeof name === "string")) {
+      generateError("모든 근무자의 이름은 문자로 입력해야 합니다.");
+    }
+  }
+
+  static #validateLength(workers) {
+    if (workers.some((name) => name.length < 1 || name.length > 5)) {
+      generateError("모든 근무자의 이름은 1자 ~ 5자 이내로 입력가능합니다.");
     }
   }
 }
