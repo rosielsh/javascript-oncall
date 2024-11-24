e2e505ef6b1ee7b5c4fc85e92883c822cf91aa89
suhwa <tnghk9611@naver.com>
2024-11-24 14:04:07 +0900

    test(MonthTest): 월은 1~12사이의 숫자로 입력받는 테스트 작성

---
+++ __tests__/models/Month.test.js
@@ -0,0 +1,17 @@
+describe("Month 클래스 테스트", () => {
+  test("월은 1~12 사이의 숫자로 입력할 수 있다", () => {
+    const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    months.forEach((month) => {
+      expect(new Month(month, "월")).not.toThrow();
+    });
+  });
+
+  test("월은 1~12 사이의 숫자가 아니면 에러가 발생한다", () => {
+    const months = [0, 13, undefined, null, NaN, {}, [], "string"];
+
+    months.forEach((month) => {
+      expect(new Month(month, "월")).not.toThrow();
+    });
+  });
+});
